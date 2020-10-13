ab42362946285fda0888bf71a57cb14025562371
AlexSousa <alex.sousa20@hotmail.com>
2020-10-13 16:16:32 -0300

    Versão Web

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles/global.css'
+
+import Routes from './routes'
+import { Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
